c9f7449f183089645e49507e763708378aafb424
milo24s1 <na5703hansitha@gmail.com>
2019-07-16 18:09:07 +0530

    add logout button change login via button icon

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
 
-dbPassword ='mongodb://nironipu:cpnb0381@ds149947.mlab.com:49947/regi'
+dbPassword ='mongodb://nironipu:cpnb0381@ds149947.mlab.com:49947/regi';
 
 module.exports = {
     mongoURI: dbPassword
